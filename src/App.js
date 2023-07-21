1055105e5d00de046425327cd3a9db651a5461f9
QueenterJuma <jumahqueenter@gmail.com>
2023-07-20 22:53:24 -0700

    Add login and signup  route in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Nav from './components/Nav';
+import Login from './components/user-sessions/login';
+import Signup from './components/user-sessions/signup';
 import './App.css';
 
 const App = () => (
@@ -9,6 +11,8 @@ const App = () => (
     <Nav />
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/user/login" element={<Login />} />
+      <Route path="/user/signup" element={<Signup />} />
     </Routes>
   </Router>
 );
