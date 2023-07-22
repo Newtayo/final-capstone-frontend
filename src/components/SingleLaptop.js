521acd75a94f7a190e0a15bce3e2d9c450366d97
QueenterJuma <jumahqueenter@gmail.com>
2023-07-21 22:19:06 -0700

    Add IoChevronForwardCircleOutline from 'react-icons/io5'

--- src/components/SingleLaptop.js
+++ src/components/SingleLaptop.js
@@ -1,1 +1,3 @@
 import { Link, useParams } from 'react-router-dom';
+// import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { IoChevronForwardCircleOutline } from 'react-icons/io5';
