939add2e589eae5c7722cb5a86990a553632c779
QueenterJuma <jumahqueenter@gmail.com>
2023-07-21 22:28:34 -0700

    Add SingleLaptop method in SingleLaptop.js file

--- src/components/SingleLaptop.js
+++ src/components/SingleLaptop.js
@@ -1,3 +1,12 @@
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 // import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 import { IoChevronForwardCircleOutline } from 'react-icons/io5';
+
+const SingleLaptop = () => {
+  return (
+    <div>SingleLaptop</div>
+  );
+};
+
+export default SingleLaptop;
