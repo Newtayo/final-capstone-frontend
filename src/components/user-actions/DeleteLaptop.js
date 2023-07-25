1aac84ff1a5ef815343b8764331c844ff531eb0c
Newtayo <akandeabdul@gmail.com>
2023-07-25 10:38:35 +0100

    Import the list of all Laptops from the store

--- src/components/user-actions/DeleteLaptop.js
+++ src/components/user-actions/DeleteLaptop.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { UseSelector } from 'react-redux/es/hooks/useSelector'
 
 const DeleteLaptop = () => {
+    const availableLaptops = useSelector((store) => store.laptops.laptops);
   return (
     <div>DeleteLaptop</div>
   )
