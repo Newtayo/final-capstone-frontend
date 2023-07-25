da42e6fcbef945966e82a5712abe2a4771976bd5
QueenterJuma <jumahqueenter@gmail.com>
2023-07-24 22:35:28 -0700

    Add state manegement variables

--- src/components/user-actions/AddLaptop.js
+++ src/components/user-actions/AddLaptop.js
@@ -4,4 +4,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { addLaptop, clear } from '../../redux/laptop/laptopSlice';
 
-  const AddLaptop = () => {}
+  const AddLaptop = () => {
+    const dispatch = useDispatch();
+
+  const [overlay, setOverlay] = useState(false);
+  const [name, setName] = useState('');
+  const [photoUrl, setPhotoUrl] = useState('');
+  const [modelYear, setModelYear] = useState('');
+  const [price, setPrice] = useState('');
+  const [romSize, setRomSize] = useState('');
+  const [ramSize, setRamSize] = useState('');
+  const [description, setDescription] = useState('');
+  
+  }
