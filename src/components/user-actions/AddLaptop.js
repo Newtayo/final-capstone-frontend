6818a272594f42061311d286b0680374cc6b1163
QueenterJuma <jumahqueenter@gmail.com>
2023-07-24 20:21:13 -0700

    Add useNavigate from react-router-dom

--- src/components/user-actions/AddLaptop.js
+++ src/components/user-actions/AddLaptop.js
@@ -1,3 +1,4 @@
 import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
