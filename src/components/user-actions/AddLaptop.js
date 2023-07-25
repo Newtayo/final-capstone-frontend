e9adb5cd73a22516aabd842886e29f259e0c9878
QueenterJuma <jumahqueenter@gmail.com>
2023-07-24 20:23:47 -0700

    Add addLaptop, clear from laptopSlice

--- src/components/user-actions/AddLaptop.js
+++ src/components/user-actions/AddLaptop.js
@@ -2,3 +2,4 @@ import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { addLaptop, clear } from '../../redux/laptop/laptopSlice';
