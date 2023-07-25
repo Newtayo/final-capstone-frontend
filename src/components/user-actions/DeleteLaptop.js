6c628fabffeb7e7a46ffce866d2252369dd85182
Newtayo <akandeabdul@gmail.com>
2023-07-25 10:56:42 +0100

    Add authorization to delete laptop

--- src/components/user-actions/DeleteLaptop.js
+++ src/components/user-actions/DeleteLaptop.js
@@ -2,46 +2,62 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom';
 import { destroyLaptop } from '../../redux/laptop/laptopSlice';
+import { useNavigation } from 'react-router-dom';
 
 const DeleteLaptop = () => {
     const availableLaptops = useSelector((store) => store.laptops.laptops);
     const dispatch = useDispatch()
-
+    const redirection = useNavigate();
+    useEffect(() => {
+        if (!isLoggedIn) {
+          setTimeout(() => {
+            redirection('/user/login');
+          }, 2000);
+        }
+      });
     const removeData = (e) => {
         const id = Number(e.target.value);
         dispatch(destroyLaptop(id));
       };
-  return (
-    <section className="delete-laptop-page">
-    <h1>Delete a laptop</h1>
-
-    {availableLaptops.length !== 0 ? (
-      <ul className="available-laptops-list">
-        {availableLaptops.map((item) => (
-          <li className="available-laptop" key={item.id}>
-            <span>
-              {item.name}
-            </span>
-            <button
-              type="button"
-              name="delete"
-              className="delete-btn"
-              value={item.id}
-              onClick={removeData}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    ) : (
-      <div className="no-items-available">
-        <div>There are no laptops available.</div>
-        <button type="button"><Link to="/"> Go back to the home page</Link></button>
-      </div>
-    )}
-  </section>
-  )
-}
+ 
+      if (isLoggedIn) {
+        return (
+          <section className="delete-laptop-page">
+            <h1>Delete a laptop</h1>
+    
+            {availableLaptops.length !== 0 ? (
+              <ul className="available-laptops-list">
+                {availableLaptops.map((item) => (
+                  <li className="available-laptop" key={item.id}>
+                    <span>
+                      {item.name}
+                    </span>
+                    <button
+                      type="button"
+                      name="delete"
+                      className="delete-btn"
+                      value={item.id}
+                      onClick={removeData}
+                    >
+                      Delete
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <div className="no-items-available">
+                <div>There are no laptops available.</div>
+                <button type="button"><Link to="/"> Go back to the home page</Link></button>
+              </div>
+            )}
+          </section>
+        );
+      }
+      return (
+        <div className="popup-message">
+          <p>Please log in to access this page</p>
+        </div>
+      );
+    };
 
 export default DeleteLaptop
