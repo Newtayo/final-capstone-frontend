5967b677d5134078374a27d8fbdb16c02f9dd805
QueenterJuma <jumahqueenter@gmail.com>
2023-07-25 17:47:54 -0700

    Add update

--- src/redux/user/sessionSlice.js
+++ src/redux/user/sessionSlice.js
@@ -1,13 +1,27 @@
-// API data
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 // Actions
-const FULLFILED = 'quickmaths-frontend/user/user-signup/FULLFILED';
-const LOGOUT = 'quickmaths-frontend/user/user-signup/LOGOUT';
-const ADD_RESERVATION = 'reservations/reseravtions/ADD_RESERVATION';
-const REMOVE_MSG = 'reservations/reservations/REMOVE_MSG';
+// const FULLFILED = 'quickmaths-frontend/user/user-signup/FULLFILED';
+// const LOGOUT = 'quickmaths-frontend/user/user-signup/LOGOUT';
+// const ADD_RESERVATION = 'reservations/reseravtions/ADD_RESERVATION';
+// const REMOVE_MSG = 'reservations/reservations/REMOVE_MSG';
 const CREATE_RESERVATION_LINK = 'http://127.0.0.1:3000/api/v1/reservation';
-const REMOVE_RESERVATIONS = 'reservations/reservations/REMOVE_RESERVATIONS';
+// const REMOVE_RESERVATIONS = 'reservations/reservations/REMOVE_RESERVATIONS';
 
+export const userSession = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await fetch(`http://127.0.0.1:3000/api/v1/${endpoint}`,{
+    method: 'POST',
+    body: JSON.stringify(obj),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const userData = await response.json();
+  if (response.status < 200 || response.status >= 300) {
+    return 'fails';
+  }
+  return userData;
+});
 // Initial state
 const initialState = {
   user: {},
@@ -17,83 +31,30 @@ const initialState = {
 };
 
 // Reducer
-const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case FULLFILED:
-      return {
+const userReducer = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(userSession.fulfilled, (state, action) => ({
         ...state,
-        ...action.payload.obj,
-      };
-
-    case LOGOUT:
-      return initialState;
-
-    case ADD_RESERVATION:
-      if (action.payload.newReservation) {
-        return {
-          ...state,
-          reservations: [...state.reservations, action.payload.newReservation],
-          creationMsg: action.payload.msg,
-        };
-      }
-      return {
+        cars: action.payload,
+      }));
+    builder.addCase(
+      userSession.rejected,
+      (state, action) => ({
         ...state,
-        creationMsg: action.payload.msg,
-      };
-
-    case REMOVE_RESERVATIONS:
-      return {
-        ...state,
-        reservations: [...state.reservations.filter((item) => item.laptop_id !== action.payload)],
-      };
-
-    case REMOVE_MSG:
-      return {
-        ...state,
-        creationMsg: action.payload,
-      };
-
-    default:
-      return state;
-  }
-};
-
-// Action Creators
-const setReservationAction = (reservation) => ({
-  type: ADD_RESERVATION,
-  payload: reservation,
-});
-
-const setMsgAction = () => ({
-  type: REMOVE_MSG,
-  payload: '',
-});
-
-const fullfiled = (obj) => ({
-  type: FULLFILED,
-  payload: { obj },
-});
-
-const logout = () => ({
-  type: LOGOUT,
-});
-
-const setRemoveReservationsAction = (id) => ({
-  type: REMOVE_RESERVATIONS,
-  payload: id,
-});
-
-const userSession = (obj, endpoint) => async (dispatch) => fetch(`http://127.0.0.1:3000/api/v1/${endpoint}`, {
-  method: 'POST',
-  body: JSON.stringify(obj),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
+        error: action.payload,
+      }),
+    );
+    builder.addCase(userSession.fulfilled, (state, action) => ({
+      ...state,
+      messages: action.payload,
+    }));
   },
-})
-  .then((res) => res.json())
-  .then((data) => {
-    dispatch(fullfiled(data));
-  });
+});
+
 
 const fetchReservation = (data) => async (dispatch) => {
   await fetch(CREATE_RESERVATION_LINK, {
@@ -114,7 +75,3 @@ const fetchReservation = (data) => async (dispatch) => {
 };
 
 export default userReducer;
-export {
-  fullfiled, userSession, fetchReservation,
-  setReservationAction, setMsgAction, logout, setRemoveReservationsAction, REMOVE_RESERVATIONS,
-};
